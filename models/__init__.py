2313360bcbeadab5ca2d3df287dab4f48e74f70f
= <=>
2023-07-17 20:36:53 -0300

    inicializando

---
+++ models/__init__.py
@@ -0,0 +1,3 @@
+from models.usuario import Usuario, TipoUsuario, EstadoUsuario, LicenciaConducir
+from models.viaje import Viaje, Pasajero, EstadoPasajero, Tracking
+from models.vehiculo import Vehiculo, TipoVehiculo, Seguro, Cedula
