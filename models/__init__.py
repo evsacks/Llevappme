25018d441653c530e36396e214b1eaecba862236
Evelyn <esacks@frd.utn.edu.ar>
2023-07-17 20:45:10 -0300

    Revert "inicializando"
    This reverts commit 2313360bcbeadab5ca2d3df287dab4f48e74f70f.

--- models/__init__.py
+++
@@ -1,3 +0,0 @@
-from models.usuario import Usuario, TipoUsuario, EstadoUsuario, LicenciaConducir
-from models.viaje import Viaje, Pasajero, EstadoPasajero, Tracking
-from models.vehiculo import Vehiculo, TipoVehiculo, Seguro, Cedula
