74377915f9a37fc8613ac17b71d8849444df2ce9
Evelyn Victoria Sacks Peikert <134658081+evsacks@users.noreply.github.com>
2023-07-18 20:42:41 -0300

    inicializando repo

---
+++ config.py
@@ -0,0 +1,27 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = '5c613dc1b1f798beae51e3b218a1cb3b2e220a4c0f643ddd506d056ceb9e3257'
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
