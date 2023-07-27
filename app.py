4de7eb0847e68844310ec2bd629613013a918228
Evelyn <esacks@frd.utn.edu.ar>
2023-07-27 13:43:21 -0300

    Flask-Script no es compatible con Flsak 2.+. Se cambia por Flask CLI

--- app.py
+++ app.py
@@ -2,14 +2,19 @@ import os
 from flask import Flask
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 
 app = Flask(__name__)
 
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
 api = Api(app)
 
+migrate = Migrate()
+migrate.init_app(app, db)
 
 @app.route('/login', methods=['GET', 'POST'])
 def Login():
--- manage.py
+++ manage.py
@@ -1,11 +1,7 @@
-from flask_script import Manager
-from flask_migrate import Migrate, MigrateCommand
-from app import app, db
+from app import app
+from flask.cli import FlaskGroup
 
-migrate = Migrate(app, db)
-manager = Manager(app)
-
-manager.add_command('db', MigrateCommand)
+cli = FlaskGroup(app)
 
 if __name__ == '__main__':
-    manager.run()
+    cli()
