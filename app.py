3b8141a9a3911ce7a28141de9aa70059c443adff
Evelyn <esacks@frd.utn.edu.ar>
2023-07-27 19:27:19 -0300

    ajustando flask-script

--- app.py
+++ app.py
@@ -2,20 +2,15 @@ import os
 from flask import Flask
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-
 
 app = Flask(__name__)
 
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 db = SQLAlchemy(app)
 api = Api(app)
 
-migrate = Migrate()
-migrate.init_app(app, db)
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def Login():
     return "Hola prueba"
--- manage.py
+++ manage.py
@@ -1,7 +1,11 @@
-from app import app
-from flask.cli import FlaskGroup
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+from app import app, db
 
-cli = FlaskGroup(app)
+migrate = Migrate(app, db)
+manager = Manager(app)
+
+manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
-    cli()
+    manager.run()
