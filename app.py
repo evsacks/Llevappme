ecbb90704f45b0645f859c77e2ecd32b828798a9
= <=>
2023-07-27 19:45:32 -0300

    inicializando repositorio

--- app.py
+++ app.py
@@ -1,16 +1,24 @@
+from datetime import datetime, timedelta
 import os
-from flask import Flask
+
+from flask import Response,Flask, request, jsonify, send_file, render_template, redirect, url_for, flash, send_from_directory, session, send_from_directory
 from flask_restful import Api
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy, Pagination
 
 app = Flask(__name__)
 
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
 api = Api(app)
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def Login():
-    return "Hola prueba"
+    hola = 'Hola Mundo'
+    return hola
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
